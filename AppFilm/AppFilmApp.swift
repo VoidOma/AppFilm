b2d5b42a57a4ed416bb4a49d8760c46eecddba23
VoidOma <MilanMatejka38@gmail.com>
2025-09-26 14:01:33 +0200

    Initial Commit

---
+++ AppFilm/AppFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFilmApp.swift
+//  AppFilm
+//
+//  Created by admin on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppFilmTests/AppFilmTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppFilmTests.swift
+//  AppFilmTests
+//
+//  Created by admin on 26/09/2025.
+//
+
+import Testing
+@testable import AppFilm
+
+struct AppFilmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
